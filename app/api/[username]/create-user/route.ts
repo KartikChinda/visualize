f2ce4f130a9427ec562ef98e3ced3adfd171d58b
Kartik Chinda <kartik.chinda@gmail.com>
2025-01-29 20:46:43 +0530

    Feat: Added API routes for fetching and inserting userInfo

---
+++ app/api/[username]/create-user/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { userSchema } from "@/app/schemas/userSchema";
+import z from "zod"; 
+
+const prisma = new PrismaClient(); 
+
+export async function POST (req: NextRequest, {params} : {params: {username: string}}) {
+    const { username } = await params; 
+    try {
+        const userData = await req.json(); 
+        userSchema.parse(userData);
+        const newUser = await prisma.user.upsert({
+            where: {username: username}, 
+            update: userData, 
+            create: userData, 
+        })
+
+        return NextResponse.json(newUser, {status: 200}); 
+    } catch (error) {
+        if(error instanceof z.ZodError) {
+            return NextResponse.json(
+                {error: "Validation failed.", details: error.errors}, 
+                {status: 400}
+            )
+        }
+        return NextResponse.json(
+            {error: "User creation failed."}, 
+            {status: 500},
+        )
+    }
+}
